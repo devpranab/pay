d5e9114641708abd8519e9d82e3ede6d9eac12e6
pranav s <dev.pranabsk@gmail.com>
2023-01-06 15:34:57 +0530

    2. Stripe Account Install Stripe and Stripe For React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import ProcessPayment from './components/ProcessPayment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <h2>Stripe Payment Gateway</h2>
+     <ProcessPayment/>
     </div>
   );
 }
---
+++ src/components/ProcessPayment.js
@@ -0,0 +1,21 @@
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_51MN87ySGo3lHFbtsqrc85fpl7rgtnZI1ytPJHHmB5SXQmWWue0CIPybNCn2kWJ8Ofsn5swTvyBbvQqcH903VEyAR00WODOkRT7');
+
+const ProcessPayment = () => {
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: '{{CLIENT_SECRET}}',
+  };
+
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      {/* <CheckoutForm /> */}
+    </Elements>
+  );
+};
+
+export default ProcessPayment;
